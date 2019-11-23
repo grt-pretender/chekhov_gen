eba26e7d05ec102687b556d6c136dfc8963842f7
Daria <dmetafrasis@gmail.com>
2019-11-23 22:18:38 +0700

    Commit new file

---
+++ day0_Hello, World.js
@@ -0,0 +1,11 @@
+//Print the following two lines of output:
+//On the first line, print Hello, World!.
+//On the second line, print the contents of parameterVariable.
+
+function greeting(parameterVariable) {
+
+    console.log('Hello, World!');
+
+    console.log(parameterVariable);
+
+}
