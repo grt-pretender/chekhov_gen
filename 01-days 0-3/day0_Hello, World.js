3376314ddda79176e0b285f0176e6402bf918bd1
Daria <dmetafrasis@gmail.com>
2019-11-24 01:57:00 +0700

    Commit new file

--- 01-days 0-3/day0_Hello, World.js
+++ 01-days 0-3/day0_Hello, World.js
@@ -1,6 +1,6 @@
 //Print the following two lines of output:
-//On the first line, print Hello, World!
-//On the second line, print the contents of parameterVariable (i.e., the argument passed to main)
+//On the first line, print Hello, World!.
+//On the second line, print the contents of parameterVariable.
 
 function greeting(parameterVariable) {
 
