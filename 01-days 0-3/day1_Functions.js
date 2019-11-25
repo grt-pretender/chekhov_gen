bf975c8d7e58cca38df3a3e45b439696c420a531
Daria <dmetafrasis@gmail.com>
2019-11-25 19:27:16 +0700

    Make a comment

--- 01-days 0-3/day1_Functions.js
+++ 01-days 0-3/day1_Functions.js
@@ -1,17 +1,10 @@
 /**
 Task
-
 Implement a function named factorial that has one parameter: an integer, n.
 It must return the value of n! (i.e., n factorial).
 
 Input Format
 Locked stub code in the editor reads a single integer, n, from stdin and passes it to a function named factorial.
-
-Constraints
-
-Output Format
-
-The function must return the value of n!.
 **/
 
 function factorial(n){
@@ -21,7 +14,6 @@ function factorial(n){
         return n * factorial(n-1);
     }
 }
-
 
 
 //Another variant using ternary operators
